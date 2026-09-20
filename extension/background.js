46cba0d2930f80bf6d67385e771082271ba19473
Pulkit <kpulkit15234@gmail.com>
2026-09-20 19:27:57 +0530

    Open toolbar form suggestions in a popup with fill and undo

--- extension/background.js
+++ extension/background.js
@@ -4,6 +4,3 @@ chrome.storage.local.setAccessLevel({ accessLevel: 'TRUSTED_CONTEXTS' });
 const credentials = createCredentialBridge({ runtime: chrome.runtime, storage: chrome.storage.local });
 credentials.listen();
 void credentials.sync();
-chrome.action.onClicked.addListener(async tab => {
-  await chrome.tabs.create({ url: chrome.runtime.getURL(`app.html?tab=${tab.id}`) });
-});
